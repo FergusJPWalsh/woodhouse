aec679615eb50ccb6c9d10bb81bfde9bc159bf27
Fergus <36740727+FergusJPWalsh@users.noreply.github.com>
2022-05-20 16:21:40 +0100

    EOL

--- make_csv.py
+++ make_csv.py
@@ -1,4 +1,5 @@
 # converts the TSV file to a CSV file to be used for the GitHub page.
+import io
 import csv
 new_data = []
 with open("riddle-arnold.tsv", "r", encoding="utf-8") as f:
@@ -7,6 +8,6 @@ with open("riddle-arnold.tsv", "r", encoding="utf-8") as f:
         row[0] = row[0].lower()
         new_row = f'"{row[0]}","<div style=""margin-left:1em"">{row[1]}</div>"'
         new_data.append(new_row)
-with open("data/riddle-arnold_for_web.csv", "w", encoding="utf-8") as g:
-    for row in new_data:
-        g.write(row+"\r\n")
+g = io.open("data/riddle-arnold_for_web.csv", "w", encoding="utf-8", newline="\n")
+for row in new_data:
+    g.write(row+"\r\n")
